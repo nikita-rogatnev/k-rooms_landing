5b2daaf3bbef595d561a53c78e6e5ad38597e752
Rogatnev Nikita <mail@rogatnev.ru>
2016-07-10 19:01:31 +0300

    form send

--- php/script.php
+++ php/script.php
@@ -1,1 +1,10 @@
+<?
+    $to = "KListov@gmail.com";
+    $subject = "Регистрация на Party Went Wrong";
+    $message = "<html><head><title>'.$subject.'</title></head>";
+    $message .= "<body><p>Имя: ".$_POST['name']."</p><p>Email: ".$_POST['email']."</p><p>Квест: Party Went Wrong</p></body></html>";
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "From: Отправитель <".$to.">\r\n";
 
+    mail($to, $subject, $message, $headers);
+?>
